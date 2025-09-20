007e5e55a50e5979fc44511c2f9e781c55979f2e
Khesir <khesir2002@gmail.com>
2025-09-20 10:55:43 +0800

    chore:Added changes from previous commit

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,23 @@
 package main
 
 import (
-	"app/internal/server"
+	server "app/internal"
 	"embed"
 	"log"
 )
 
 var embeddedFrontend embed.FS
 
+// var for all necessary internal values
+// Create a app struct
+func init() {
+	// nessary inits
+	// connectt to db
+	// static assets file system
+	// checker for db schema
+	// upgrades?
+	// 
+}
 func main() {
 	if err := server.Start(":8080", embeddedFrontend); err != nil {
 		log.Println("server stopped:", err)
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,15 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
+// Vite supports loading custom .env files with the --mode flag.
+export default defineConfig(({mode}) => {
+  const env = loadEnv(mode, process.cwd() + "/env", "")
+  return {
   plugins: [react()],
+  define: {
+    "process.env": env,
+  },
   server: {
     proxy: {
       "/api": {
@@ -13,4 +19,5 @@ export default defineConfig({
       }
     }
   }
+}
 })
